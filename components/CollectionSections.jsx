4a08f40b7dc83ce0d6831c4b19da89adba01ea80
Josh <khourydev@gmail.com>
2024-03-24 18:13:13 -0400

    up

--- components/CollectionSections.jsx
+++ components/CollectionSections.jsx
@@ -1,27 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { fetchProductsFromCollection } from "../utils/shopify";
-import {
-  Typography,
-  Card,
-  CardContent,
-  Button,
-  Box,
-  useTheme,
-  useMediaQuery,
-  CardActionArea,
-} from "@mui/material";
+import { Typography, Card, CardContent, Button, Box } from "@mui/material";
 import Image from "next/image";
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import Slider from "react-slick";
 
 const CollectionProductsCustomCarousel = () => {
   const [products, setProducts] = useState([]);
-  const [startIndex, setStartIndex] = useState(0);
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
-  const isTablet = useMediaQuery(theme.breakpoints.between("sm", "md"));
-  const slidesToShow = isMobile ? 1 : isTablet ? 2 : 4;
-  const gap = 16; // Adjust the gap size as needed
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -38,148 +22,76 @@ const CollectionProductsCustomCarousel = () => {
     fetchProducts();
   }, []);
 
-  const handleNext = () => {
-    setStartIndex((prevIndex) => {
-      let nextIndex = prevIndex + 1;
-      const maxStartIndex = products.length - slidesToShow;
-      if (nextIndex > maxStartIndex) {
-        nextIndex = 0;
-      }
-      return nextIndex;
-    });
-  };
-
-  const handlePrev = () => {
-    setStartIndex((prevIndex) => {
-      let newIndex = prevIndex - 1;
-      const maxStartIndex = products.length - slidesToShow;
-      if (newIndex < 0) {
-        newIndex = maxStartIndex;
-      }
-      return newIndex;
-    });
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignContent: "center",
-        margin: "auto",
-        padding: "20px",
-        margin: "auto",
-      }}
-    >
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignContent: "center",
-          margin: "auto",
-          padding: "20px",
-          margin: "auto",
-        }}
-      >
-        <Typography variant="h4" gutterBottom>
-          Products from the "10 and under" Collection
-        </Typography>
-      </Box>
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          margin: isMobile ? "initial" : "auto", // Apply 'margin: auto' only on non-mobile devices
-        }}
-      >
-        <Button onClick={handlePrev}>
-          <ArrowBackIosIcon />
-        </Button>
-        <Box
-          sx={{
-            overflow: "hidden",
-            width: `100%`, // Full width of the container
-            display: "flex",
-            marginLeft: `${gap}px`, // Consistent gap
-            marginRight: `${gap}px`, // Consistent gap
-          }}
-        >
-          <Box
-            sx={{
-              display: "flex",
-              transition: "transform 0.5s ease",
-              transform: `translateX(-${(100 / slidesToShow) * startIndex}%)`, // Adjust based on the visible slides and current index
-            }}
-          >
-            {products.map((product, index) => (
-              <Box
-                key={index}
-                sx={{
-                  width: `calc(${100 / slidesToShow}% - ${gap}px)`, // Width based on slidesToShow
-                  flexShrink: 0, // Prevent flexbox from shrinking items
-                  padding: `0 ${gap / 2}px`, // Half gap on each side for spacing
-                }}
-              >
-                <Card
-                  raised
-                  sx={{
-                    maxWidth: "200px", // Ensure card doesn't overflow its container
-                    minHeight: "200px",
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "space-between",
-                    "&:hover": { boxShadow: "0 8px 16px 0 rgba(0,0,0,0.2)" },
-                  }}
-                >
-                  <Image
-                    src={product.imageUrl}
-                    alt={product.name}
-                    layout="responsive"
-                    width={400}
-                    height={300}
-                    objectFit="cover"
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                      {product.name}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      {product.description}
-                    </Typography>
-                    {/* Display the price */}
-                    <Typography variant="body2" color="text.secondary">
-                      Price: {product.minVariantPrice.amount}{" "}
-                      {product.minVariantPrice.currencyCode}
-                    </Typography>
-                  </CardContent>
-                  <CardActionArea sx={{ borderRadius: 0 }}>
-                    <Button
-                      size="large"
-                      fullWidth
-                      variant="contained"
-                      sx={{
-                        width: "100%",
-                        borderRadius: 0,
-                        fontWeight: "bold",
-                        backgroundColor: "#1661C2",
-                        "&:hover": { backgroundColor: "orangered" },
-                      }}
-                    >
-                      View Item
-                    </Button>
-                  </CardActionArea>
-                </Card>
-              </Box>
-            ))}
+    <Box sx={{ padding: "20px", margin: "auto" }}>
+      <Typography variant="h4" gutterBottom>
+        Products from the "10 and under" Collection
+      </Typography>
+      <Slider {...settings}>
+        {products.map((product, index) => (
+          <Box key={index} sx={{ padding: "10px" }}>
+            <Card raised sx={{ maxWidth: "250px", minHeight: "200px" }}>
+              <Image
+                src={product.imageUrl}
+                alt={product.name}
+                layout="responsive"
+                width={400}
+                height={300}
+                objectFit="cover"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {product.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {product.description}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Price: {product.minVariantPrice.amount}{" "}
+                  {product.minVariantPrice.currencyCode}
+                </Typography>
+              </CardContent>
+              <Button size="large" fullWidth variant="contained">
+                View Item
+              </Button>
+            </Card>
           </Box>
-        </Box>
-        <Button onClick={handleNext}>
-          <ArrowForwardIosIcon />
-        </Button>
-      </Box>
+        ))}
+      </Slider>
     </Box>
   );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,6 @@
 import "@/styles/globals.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
